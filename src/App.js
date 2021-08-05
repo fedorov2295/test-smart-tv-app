9cda69d27bb1d36c360414ade2c1c7c6ad2d14cf
SERGEI FEDOROV <72577233+fedorov2295@users.noreply.github.com>
2021-08-05 21:46:25 +0300

    Fixing router error

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import classes from './App.module.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import MainPage from './components/MainPage.jsx/MainPage';
 import Checkout from './components/Checkout/Checkout';
 
 function App() {
   return (
     <div className={classes.main}>
-      <BrowserRouter>
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route path="/checkout" component={Checkout} />
           <Route path="/" component={MainPage} />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
